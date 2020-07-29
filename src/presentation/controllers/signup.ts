6890fec6907bc22128af42e0341a9eeb7f2faa6d
Aryel de Sá <aryel.santos@spassu.com>
2020-07-29 20:28:42 -0300

    feat: add password as required field

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,7 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password']
     let response
     requiredFields.forEach((field) => {
       if (!httpRequest.body[field]) {
