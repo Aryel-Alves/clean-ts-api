cb782bcf8769619927b10cef709b6ba2e45360ac
Aryel de Sá <aryel.santos@spassu.com>
2020-08-07 10:45:39 -0300

    feat: ensure logControllerDecorator return same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -28,7 +28,7 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Log Controller Decorator', () => {
-  test('Should call  controller handle method', async () => {
+  test('Should call controller handle method', async () => {
     const { controllerStub, sut } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
     const httpRequest = {
@@ -42,5 +42,23 @@ describe('Log Controller Decorator', () => {
 
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  test('Should return the same result as the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httResponse = await sut.handle(httpRequest)
+    expect(httResponse).toEqual({
+      statusCode: 200,
+      body: { name: 'Aryel' }
+    })
   })
 })
