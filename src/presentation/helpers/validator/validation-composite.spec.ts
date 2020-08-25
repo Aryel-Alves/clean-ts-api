c27c2bf2269ed4f3a0048bcb0980140c9f86ca39
Aryel de Sá <aryel.santos@spassu.com>
2020-08-25 20:45:39 -0300

    test: ensure validation composite not return if succeeds

--- src/presentation/helpers/validator/validation-composite.spec.ts
+++ src/presentation/helpers/validator/validation-composite.spec.ts
@@ -21,18 +21,24 @@ const makeSut = (): SutTypes => {
   return { sut, validationStubs }
 }
 describe('Validation Composite', () => {
-  test('Show return an error if any validation fails', () => {
+  test('Should return an error if any validation fails', () => {
     const { sut, validationStubs } = makeSut()
     jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
   })
 
-  test('Show return the first error if more then one validation fails', () => {
+  test('Should return the first error if more then one validation fails', () => {
     const { sut, validationStubs } = makeSut()
     jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
     jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
   })
 })
