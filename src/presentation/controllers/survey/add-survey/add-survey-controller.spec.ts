f71d7bc7708e5c1a9da5a4f6d5adc064eda766da
Aryel de Sá <aryel.santos@spassu.com>
2020-09-17 12:42:12 -0300

    feat: create add-survey controller

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -0,0 +1,48 @@
+import { HttpRequest } from './add-survey-controller-protocols'
+import { AddSurveyController } from './add-survey-controller'
+import { Validation } from '../../../protocols'
+import { badRequest } from '../../../helpers/http/http-helper'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    question: 'any_question',
+    answers: [{ image: 'any_image', answer: 'any_answer' }]
+  }
+})
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+interface SutTypes{
+  sut: AddSurveyController
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new AddSurveyController(validationStub)
+  return { sut, validationStub }
+}
+
+describe('AddSurvey Controller', () => {
+  test('Should call validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    const httRequest = makeFakeRequest()
+    await sut.handle(httRequest)
+    expect(validateSpy).toHaveBeenCalledWith(httRequest.body)
+  })
+
+  test('Should return 400 if validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
+  })
+})
---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -0,0 +1,17 @@
+import { badRequest } from '../../../helpers/http/http-helper'
+import { Validation } from '../../../protocols'
+import { Controller, HttpRequest, HttpResponse } from './add-survey-controller-protocols'
+
+export class AddSurveyController implements Controller {
+  constructor (
+    private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
+    return await Promise.resolve(null)
+  }
+}
