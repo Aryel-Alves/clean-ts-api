aef7583faf08f1fa9d00f08ded049cb41ff9a7a7
Aryel de Sá <aryel.santos@spassu.com>
2020-09-27 12:50:34 -0300

    feat: add dbLoadAccountByToken

---
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -0,0 +1,22 @@
+import { AccountModel } from '../../../domain/models/account'
+import { LoadAccountByToken } from '../../../domain/useCases/load-account-by-token'
+import { Decrypter } from '../../protocols/cryptography/decrypter'
+import { LoadAccountByTokenRepository } from '../../protocols/db/account/load-account-by-token-repository'
+
+export class DbLoadAccountByToken implements LoadAccountByToken {
+  constructor (
+    private readonly decrypter: Decrypter,
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
+  ) {}
+
+  async load (accessToken: string, role?: string): Promise<AccountModel> {
+    const token = await this.decrypter.decrypt(accessToken)
+    if (token) {
+      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+      if (account) {
+        return account
+      }
+    }
+    return null
+  }
+}
