0509f475a21c9fd10f0f7559457c6a4cec9f22c5
Aryel de Sá <aryel.santos@spassu.com>
2020-10-27 21:42:00 -0300

    docs: add loadSurveyResults path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -2,7 +2,7 @@ export const surveyResultPath = {
   put: {
     security: [{ apiKeyAuth: [] }],
     tags: ['Enquete'],
-    summary: 'API para salver resposta de uma enquete',
+    summary: 'API para salvar resposta de uma enquete',
     parameters: [{
       in: 'path',
       name: 'surveyId',
@@ -18,6 +18,32 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: { $ref: '#/components/forbidden' },
+      404: { $ref: '#/components/notFound' },
+      500: { $ref: '#/components/serverError' }
+    }
+  },
+  get: {
+    security: [{ apiKeyAuth: [] }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: { type: 'string' }
+    }],
     responses: {
       200: {
         description: 'Sucesso',
